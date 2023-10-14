a3b695935cbc0eda6be3caab8f641775ef1c8729
nkartikaxioned <kartikn@axioned.com>
2023-10-14 15:11:14 +0530

    js form validation add

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const hamBar1 = document.querySelector('.bar.first'),
   companyType = document.querySelector('.companyType'),
   country = document.querySelector('.country'),
   workemail = document.querySelector('.workemail'),
-  subscribe = document.querySelector('.subscribe'),
+  subscribe = document.querySelectorAll('.subscribe'),
   submitButton = document.querySelector('.submit-btn'),
   navBar = document.querySelector('nav'),
   mainBody = document.querySelector('main'),
@@ -23,30 +23,126 @@ hamburger.addEventListener('click', () => {
   navBar.classList.toggle('active');
 })
 
-returnbtn.addEventListener('click', () => {
-  window.scrollTo({ top: 0 });
-})
+returnbtn.addEventListener('click', () => { window.scrollTo({ top: 0 }); })
 
-submitButton.addEventListener('click', () => {
-  console.log('sub')
-  validateForm();
-})
+submitButton.addEventListener('click', () => { validateForm(); })
 
+fieldName.addEventListener('blur', () => {
+  validateName();
+});
+
+fieldSurname.addEventListener('blur', () => {
+  validateLastName()
+});
+
+fieldPosition.addEventListener('blur', () => {
+  validatePosition()
+});
+
+fieldcompany.addEventListener('blur', () => {
+  validateCompany()
+});
+
+companyType.addEventListener('blur', () => {
+  validateCompanyType()
+});
+
+country.addEventListener('blur', () => {
+  validateCountry()
+});
+
+workemail.addEventListener('blur', () => {
+  validateEmail()
+});
 
 function validateForm() {
-  console.log('validateForm')
   validateName()
-  function validateName() {
-    if(fieldName.innerText.value == '' || fieldName.value.length < 3) {
-      console.log(fieldName.value)
-        fieldName.classList.add('error')
-    }
-    if(fieldName.innerText.value !== '' || fieldName.value.length > 3){
-      fieldName.classList.remove('error')
-    }
+  validateLastName()
+  validatePosition()
+  validateCompany()
+  validateCompanyType()
+  validateCountry()
+  validateEmail()
+  validateSubscribtion()
+}
+
+function validateName() {
+  const nameValue = fieldName.value.trim();
+  if (nameValue === '' || nameValue.length < 3 || /^[a-zA-Z ]$/.test(nameValue)) {
+    fieldName.classList.add('error');
+  } else {
+    fieldName.classList.remove('error');
   }
 }
 
+function validateLastName() {
+  const lastname = fieldSurname.value.trim();
+  if (lastname === '' || lastname.length < 3 || /^[a-zA-Z ]$/.test(lastname)) {
+    fieldSurname.classList.add('error');
+  } else {
+    fieldSurname.classList.remove('error');
+  }
+}
+
+function validatePosition() {
+  const position = fieldPosition.value.trim();
+  if (position === '' || /^[a-zA-Z ]$/.test(position)) {
+    fieldPosition.classList.add('error');
+  } else {
+    fieldPosition.classList.remove('error');
+  }
+}
+
+function validateCompany() {
+  const company = fieldcompany.value.trim();
+  if (company === '' || company === null) {
+    fieldcompany.classList.add('error');
+  } else {
+    fieldcompany.classList.remove('error');
+  }
+}
+
+function validateCompanyType() {
+  const companytype = companyType.value;
+  if(companytype === 'select') {
+    companyType.classList.add('error');
+  } else {
+    companyType.classList.remove('error');
+  }
+}
+
+function validateCountry() {
+  const countrytype = country.value;
+  if(countrytype === 'select') {
+    country.classList.add('error');
+  } else {
+    country.classList.remove('error');
+  }
+}
+
+function validateEmail() {
+  const email = workemail.value.trim();
+  const validRegex =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  if( email === '' || email.match(validRegex) == null ) {
+    workemail.classList.add('error');
+  } else {
+    workemail.classList.remove('error');
+  }
+}
+
+function validateSubscribtion() {
+    console.log(subscribe[0].checked)
+    console.log(subscribe[1].checked)
+    if(!subscribe[0].checked || !subscribe[1].checked){
+      subscribe.forEach((sub, index) => {
+         sub.classList.add('error')
+      })
+    } else {
+      subscribe.forEach((sub, index) => {
+        sub.classList.remove('error')
+     })
+    }
+}
 
 
 
